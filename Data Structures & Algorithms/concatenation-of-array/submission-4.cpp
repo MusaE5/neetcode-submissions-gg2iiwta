9dc887de80e43de9b4ec218e9d83b0ad68843c58
Musa Elashaal <elashaalmusa@gmail.com>
2026-08-15 14:01:58 -0400

    Add: concatenation-of-array - submission-4

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> result = nums;
+
+        for(int i: nums){
+            result.push_back(i);
+        }
+
+        return result;
+        
+    }
+};
